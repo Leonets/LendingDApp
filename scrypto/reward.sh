32599a6fb4faf30dbeaba9a7d9dc7b259e4eb350
Leonardo Battagli <lbattagli@florence-consulting.it>
2023-12-02 17:21:51 +0100

    interest on lendings

---
+++ scrypto/reward.sh
@@ -0,0 +1,105 @@
+set -e
+
+export xrd=resource_sim1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxakj8n3
+
+echo "Resetting environment"
+resim reset
+export account=$(resim new-account | sed -nr "s/Account component address: ([[:alnum:]_]+)/\1/p")
+echo "Account = " $account
+echo "XRD = " $xrd
+
+echo "Publishing dapp"
+export lendingapp_package=$(resim publish . | sed -nr "s/Success! New Package: ([[:alnum:]_]+)/\1/p")
+echo "Package = " $lendingapp_package
+
+output=`resim call-function $lendingapp_package LendingDApp instantiate_lending_dapp 5 10 LND 1728 timebased 1000 | awk '/Component: |Resource: / {print $NF}'`
+export component=`echo $output | cut -d " " -f1`
+export owner_badge=`echo $output | cut -d " " -f2`
+export admin_badge=`echo $output | cut -d " " -f3`
+export staff_badge=`echo $output | cut -d " " -f4`
+export benefactor_badge=`echo $output | cut -d " " -f5`
+export lending_token=`echo $output | cut -d " " -f6`
+export lnd_manager=`echo $output | cut -d " " -f7`
+
+
+export component_test=component_sim1cptxxxxxxxxxfaucetxxxxxxxxx000527798379xxxxxxxxxhkrefh
+
+echo 'component = '$component
+echo 'owner_badge = '$owner_badge
+echo 'admin_badge = '$admin_badge
+echo 'staff_badge = '$staff_badge
+echo 'lending_token = ' $lending_token
+echo 'lnd_manager = ' $lnd_manager
+echo 'benefactor_badge = ' $benefactor_badge
+
+echo ' '
+echo 'account = ' $account
+echo 'xrd = ' $xrd
+echo 'test faucet for lock fee = ' $component_test
+echo ' '
+
+echo '>>> Fund Main Vault'
+
+resim run rtm/fund_main_pool.rtm
+resim run rtm/fund_main_pool.rtm
+# main pool 200
+resim run rtm/fund_main_pool_4000.rtm
+# main pool 4200
+
+echo '>>> Donate'
+
+resim run rtm/fund.rtm
+resim run rtm/fund.rtm
+# donations 200
+
+echo '>>> Register'
+
+resim run rtm/register.rtm
+
+resim show $account
+
+echo '>>> Lend tokens'
+
+resim set-current-epoch 1
+#resim call-method ${component} lend_tokens $xrd:100
+resim run rtm/lend_tokens.rtm
+
+resim show $account
+
+echo '>>> Set Reward 4 at epoch 100'
+resim set-current-epoch 100
+resim run rtm/set_reward_4.rtm
+
+echo '>>> Set Reward 8 at epoch 200'
+resim set-current-epoch 200
+resim run rtm/set_reward_8.rtm
+
+echo '>>> Set Reward 12 at epoch 300'
+resim set-current-epoch 300
+resim run rtm/set_reward_12.rtm
+
+echo '>>> Show Reward Values along epochs'
+resim run rtm/lend_reward.rtm
+
+resim set-current-epoch 400
+
+echo '>>> Takes back'
+
+# 1 epoch = 5min
+# 12 epoch = 1h
+# 264 epoch = 1d
+# 7920 epoch = 1month
+# 96.360 epoch = 1year
+
+# resim set-current-epoch 96360
+#resim call-method ${component} takes_back $lending_token:100
+resim run rtm/takes_back.rtm
+# fee 10
+
+echo '>>> Takes back remaining'
+
+resim run rtm/takes_back_20.rtm
+# fee 10 
+# main pool -5
+
+resim show $component
--- scrypto/src/lib.rs
+++ scrypto/src/lib.rs
@@ -65,7 +65,7 @@ pub struct LenderData {
 }
 
 #[derive(NonFungibleData, ScryptoSbor, Clone)]
-struct CreditScore {
+pub struct CreditScore {
     #[mutable]
     amount_borrowed: Decimal,
     #[mutable]
@@ -88,6 +88,7 @@ mod lending_dapp {
             borrow => PUBLIC;
             repay => PUBLIC;
             borrow_interest => PUBLIC;
+            lend_reward => PUBLIC;
             pools => restrict_to: [admin, OWNER];
             fund_main_pool => restrict_to: [admin, OWNER];
             set_reward => restrict_to: [admin, OWNER];
@@ -412,8 +413,9 @@ mod lending_dapp {
             let allowed_amount = lender_data.amount / 5;
             info!("Minimum amount : {:?} ", allowed_amount);  
 
+            let amount_to_be_returned = refund.amount();
             assert!(
-                refund.amount() >= allowed_amount,
+                amount_to_be_returned >= allowed_amount,
                 "You cannot get back less than 20% of your loan!"
             );
 
@@ -428,32 +430,58 @@ mod lending_dapp {
             //calculate reward
             let mut amount_returned = dec!(0);
 
-            // Parse the string into the Reward enum
-            match Reward::from_str(&self.reward_type) {
+            //calculate interest
+            let current_epoch = Runtime::current_epoch().number(); 
+            let length = current_epoch - lender_data.start_lending_epoch.number();
+
+            //calculate interest to be repaied with specified reward type 
+            let interest_totals = match Reward::from_str(&self.reward_type) {
                 Ok(reward) => {
                     // Match statement to handle different enum variants
                     match reward {
                         Reward::Fixed => {
                             info!("Handle Fixed reward logic here");
                             // Do something specific for Fixed reward
-                            amount_returned = num_xrds_to_return + (num_xrds_to_return*self.reward/100);
+                            num_xrds_to_return*self.reward/100
                         }
                         Reward::TimeBased => {
-                            info!("Handle TimeBased reward logic here");
+                            info!("Handle TimeBased logic here from epoch {} to epoch {} applied to capital {}" , lender_data.start_borrow_epoch.number(), current_epoch, amount_to_be_returned);
                             // Do something specific for TimeBased reward
-                            let length = Runtime::current_epoch().number() - lender_data.start_lending_epoch.number();
+
+                            let mut total_amount = dec!(0);
+                            let mut first_epoch = Decimal::from(lender_data.start_lending_epoch.number());
+                            let mut last_value = dec!(0);
+                            for (key, value) in self.interest_for_lendings.range(
+                                    Decimal::from(lender_data.start_lending_epoch.number())..Decimal::from(current_epoch)
+                                ) {
+                                
+                                let internal_length = key-first_epoch;
+                                info!("epoch: {}, interest %: {}, length of the period: {}", key, value, internal_length);
+                                let accumulated_interest = Self::calculate_interest(Decimal::from(internal_length), self.reward, amount_to_be_returned); 
+                                total_amount = total_amount + accumulated_interest;
+                                info!("Adding accumulated_interest {} for the period, totalling {} from epoch {} until epoch {} ", accumulated_interest, total_amount, first_epoch, key);
+                                first_epoch = key;
+                                last_value = value; 
+                            }
+                            //TODO need to add the last run from first_epoch to current epoch
+                            let last = current_epoch - first_epoch;
+                            let accumulated_interest = Self::calculate_interest(Decimal::from(last), self.reward, amount_to_be_returned); 
+                            total_amount = total_amount + accumulated_interest;
+                            info!("Adding accumulated_interest {} for the period, totalling {} from epoch {} until epoch {} ", accumulated_interest, total_amount, first_epoch, current_epoch);
                             
-                            let interest = Self::calculate_interest(Decimal::from(length), self.reward, num_xrds_to_return);
-                            amount_returned = num_xrds_to_return + interest;
-                            info!("Calculated interest {:?} ", interest);  
+                            // interest_totals = Self::calculate_interest(Decimal::from(length), self.interest, amount_returned);
+                            total_amount
                         }
                     }
                 }
                 Err(()) => {
                     println!("Invalid reward string");
                     // Handle invalid input here
+                    dec!(0)
                 }
-            }
+            };
+            info!("Calculated interest {} ", interest_totals);
+            amount_returned = num_xrds_to_return + interest_totals;
 
             //give back XRD token plus reward %
             info!("XRD tokens given back: {:?} ", amount_returned);  
@@ -562,17 +590,15 @@ mod lending_dapp {
             let interest = Self::calculate_interest(Decimal::from(length), self.interest, amount_returned);
             // fixed interest calculator (lender_data.borrow_amount*self.interest/100)
 
-            //calculate interest to be repaied
-            let mut interest_totals = dec!(0);
-            // Parse the string into the Reward enum
-            match Reward::from_str(&self.reward_type) {
+            //calculate interest to be repaied based on the reward type
+            let interest_totals = match Reward::from_str(&self.reward_type) {
                 Ok(reward) => {
                     // Match statement to handle different enum variants
                     match reward {
                         Reward::Fixed => {
                             info!("Handle Fixed reward logic here");
                             // Do something specific for Fixed reward
-                            interest_totals = lender_data.borrow_amount*self.interest/100;
+                            lender_data.borrow_amount*self.interest/100
                         }
                         Reward::TimeBased => {
                             info!("Handle TimeBased logic here from epoch {} to epoch {} applied to capital {}" , lender_data.start_borrow_epoch.number(), current_epoch, amount_returned);
@@ -584,7 +610,7 @@ mod lending_dapp {
                             for (key, value) in self.interest_kv.range(
                                     Decimal::from(lender_data.start_borrow_epoch.number())..Decimal::from(current_epoch)
                                 ) {
-                                
+                                 
                                 let internal_length = key-first_epoch;
                                 info!("epoch: {}, interest %: {}, length of the period: {}", key, value, internal_length);
                                 let accumulated_interest = Self::calculate_interest(Decimal::from(internal_length), self.interest, amount_returned); 
@@ -598,16 +624,19 @@ mod lending_dapp {
                             let accumulated_interest = Self::calculate_interest(Decimal::from(last), self.interest, amount_returned); 
                             total_amount = total_amount + accumulated_interest;
                             info!("Adding accumulated_interest {} for the period, totalling {} from epoch {} until epoch {} ", accumulated_interest, total_amount, first_epoch, current_epoch);
-                            
-                            interest_totals = Self::calculate_interest(Decimal::from(length), self.interest, amount_returned);
+                        
+                            // interest_totals = Self::calculate_interest(Decimal::from(length), self.interest, amount_returned);
+                            total_amount
                         }
                     }
                 }
                 Err(()) => {
                     println!("Invalid reward string");
                     // Handle invalid input here
+                    dec!(0)
                 }
-            }
+            };
+
             //calculate interest over the borrowing period
             info!("Calculated interest {:?} ", interest_totals);  
             let amount_to_be_returned = lender_data.borrow_amount + interest_totals;
@@ -686,6 +715,12 @@ mod lending_dapp {
 
         pub fn borrow_interest(&mut self, start_epoch: Decimal, end_epoch: Decimal) {
             for (key, value) in self.interest_kv.range(start_epoch..end_epoch) {
+                info!("key: {}, value: {}", key, value);
+            }
+        }
+
+        pub fn lend_reward(&mut self, start_epoch: Decimal, end_epoch: Decimal) {
+            for (key, value) in self.interest_for_lendings.range(start_epoch..end_epoch) {
                 info!("key: {}, value: {}", key, value);
             }
         }
